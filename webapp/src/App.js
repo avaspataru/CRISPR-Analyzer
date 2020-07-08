15eb675708ad1805f4face8598c93ec8bc865aa3
avaspataru <ava.spataru@yahoo.com>
2020-07-08 17:32:43 +0100

    included datasets

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -4,12 +4,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
 
 var tools = require('./tools.json');
-
+var datasets = require('./datasets.json');
 
 function App() {
 
   const [selectedTool, setSelectedTool] = useState({});
   const [hasSelectedTool, setHasSelectedTool] = useState(false);
+
+  const [selectedData, setSelectedData] = useState({});
+  const [hasSelectedData, setHasSelectedData] = useState(false);
 
   return (
     <div className="app">
@@ -47,21 +50,44 @@ function App() {
 
                 {(hasSelectedTool) ? <div><p className="ava-descript-header">Description:</p> {selectedTool.description} </div> : <div></div>}
                 {(hasSelectedTool) ? <div><p className="ava-descript-header">Source:</p> <a href={selectedTool.link}>{selectedTool.link}</a> </div> : <div></div>}
+                <div className="ava-info"><p>The model you select will be used to calculate SHAP values of the features it considers. These may differ from model to model.</p></div>
                 </p>
+
+                <hr></hr>
 
                 <div class="form-group row">
                   <label><p className="ava-bold"> Dataset: </p> </label>
                   <div className="ava-select-cont">
-                    <select class="form-control form-control-sm">
-                      <option>Xu</option>
-                      <option>Doench</option>
+                    <select class="form-control form-control-sm"  onChange={(e) => {setSelectedData(datasets[e.target.value]); setHasSelectedData(true)}}>
+                    <option value={-1}>Please select a dataset...</option>
+                    {
+                      datasets.map((dataset,index) => <option value={index}>{dataset.name}</option>)
+                    }
                     </select>
                   </div>
                 </div>
 
                 <p className="ava-description">
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                {(hasSelectedData)
+
+                   ?
+                   <a href={selectedData.link} download>
+                   <div>
+                       <p>
+                       <div className="ava-download">
+                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                           <path fill-rule="evenodd" d="M.5 8a.5.5 0 0 1 .5.5V12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8.5A.5.5 0 0 1 .5 8z"/>
+                           <path fill-rule="evenodd" d="M5 7.5a.5.5 0 0 1 .707 0L8 9.793 10.293 7.5a.5.5 0 1 1 .707.707l-2.646 2.647a.5.5 0 0 1-.708 0L5 8.207A.5.5 0 0 1 5 7.5z"/>
+                           <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 1z"/>
+                          </svg>
+                        </div>
+                        Download a copy of the dataset.
+                        </p>
+                    </div>
+                    </a>
+
+                   : <div></div>}
+                 <div><p> The data will be used to determine the SHAP values for the features in the selected model. Each data point will have it's own SHAP values for each feature. </p></div>
                 </p>
 
 

9a5886dd05f3db1a71af2d06e92a680700c428f3
avaspataru <ava.spataru@yahoo.com>
2020-08-21 15:31:01 +0300

    logo and download message

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -8,6 +8,8 @@ import Form from 'react-bootstrap/Form';
 
 import axios from 'axios';
 
+import logo from './img/logo.png'; // with import
+
 var tools = require('./tools.json');
 var datasets = require('./datasets.json');
 
@@ -167,7 +169,7 @@ function App() {
     <div className="app">
 
       <div className="ava-container-nav">
-            <h2 class="text-center">CRISPR Analyzer</h2>
+            <img src={logo} className="ava-logo" />
       </div>
 
       <div className="ava-left-container">
@@ -358,7 +360,20 @@ function App() {
       }
 
       {(plotLoaded && !plotLoading)
-        ? <div className="ava-center"> <img src={"data:image/png;base64,"+plot}  alt="my plots" /> </div>
+        ? <div className="ava-center">
+
+              <a href={"data:image/png;base64,"+plot} download>
+                <div className="ava-download-button">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M.5 8a.5.5 0 0 1 .5.5V12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V8.5a.5.5 0 0 1 1 0V12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8.5A.5.5 0 0 1 .5 8z"/>
+                  <path fill-rule="evenodd" d="M5 7.5a.5.5 0 0 1 .707 0L8 9.793 10.293 7.5a.5.5 0 1 1 .707.707l-2.646 2.647a.5.5 0 0 1-.708 0L5 8.207A.5.5 0 0 1 5 7.5z"/>
+                  <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0v-8A.5.5 0 0 1 8 1z"/>
+                  </svg>
+                </div>
+              </a>
+              <img src={"data:image/png;base64,"+plot}  alt="my plots" />
+
+          </div>
         : <div></div>
       }
       </Card>
